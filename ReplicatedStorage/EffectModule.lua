169198383720b80346b7e945169abe914accf728
Emi <183315760+cahexis@users.noreply.github.com>
2025-12-04 10:48:39 -0600

    Update EffectModule.lua

--- ReplicatedStorage/EffectModule.lua
+++ ReplicatedStorage/EffectModule.lua
@@ -1,34 +1,36 @@
 local module = {}
 
+-- the module handles the damage, and has a half finished test on poison damage and healing.
 
-
-module.Damage = function(player, damage)
-	local oldhp = player.CurrentHp.Value
+module.Damage = function(hpstat, damage)
+	local oldhp = hpstat.Value
 	local damagetaken = damage
 	local newhp = oldhp - damage
-	player.CurrentHp.Value = newhp
+	hpstat.Value = newhp
 end
 
 
-module.Heal = function(player, heal)
-	local oldhp = player.CurrentHp.Value
+module.Heal = function(hpstat, heal)
+	local oldhp = hpstat.Value
 	local healing = heal
 	local newhp = healing + oldhp
 	if newhp > 100 then
 		newhp = 100
 	end
-	player.CurrentHp.Value = newhp
+	hpstat.Value = newhp
 end
 	
-module.PoisonEffect = function(player, duration)
+module.PoisonEffect = function(hpstat, duration)
 	local timer = duration
 	while timer > 0 do
 		wait(1)
 		print("test")
-		module.Damage(player, 1)
+		module.Damage(hpstat, 1)
 		timer = timer - 1
 	end
 end
+
+return module
 
 
 
