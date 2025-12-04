1a94ed69e4eb32737101eabb064437d5e035ebfa
Emi <183315760+cahexis@users.noreply.github.com>
2025-12-04 10:48:01 -0600

    Update HpGui.client.lua

--- StarterCharacterScripts/StarterPlayerScripts/HpGui.client.lua
+++ StarterCharacterScripts/StarterPlayerScripts/HpGui.client.lua
@@ -4,14 +4,14 @@ local healthGui = client.PlayerGui:WaitForChild("HealthGui")
 local currenthpgui = healthGui.Frame.CurrentHealth
 local rep = game:GetService("ReplicatedStorage")
 local remote = rep.HpChange
-local hp = client.Character.CurrentHp
+local hp = client:WaitForChild("HpStat")
 local module = require(rep.EffectModule)
 local oldhp = hp.Value
 local dmgsound = script.Damage
 local healsound = script.Heal
 currenthpgui.Text = hp.Value
 
-hp.Changed:Connect(function(newhp)
+hp.Changed:Connect(function(newhp) -- watches for changes in hp in order to display it on the client.
 	local healthdiff = newhp - oldhp
 	if newhp < oldhp then
 		print("took"..healthdiff.." damage")
